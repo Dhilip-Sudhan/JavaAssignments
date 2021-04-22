ab2b10d338e2361534e580629ff68f3d5a3c2ca0
Dhilip-Sudhan <82998565+Dhilip-Sudhan@users.noreply.github.com>
2021-04-22 22:25:47 +0530

    Update ToRemoveDuplicates.java
    updated

--- ToRemoveDuplicates.java
+++ ToRemoveDuplicates.java
@@ -5,7 +5,6 @@ public class ToRemoveDuplicates {
 	public static void main(String[] args) {
 		String text = "We learn java basics as part of java sessions in java week1";
 		String[] words = text.split(" ");
-		int count = 0;
 		
 		for (int i = 0; i < words.length; i++) {
 			for (int j = i+1; j < words.length; j++) {
